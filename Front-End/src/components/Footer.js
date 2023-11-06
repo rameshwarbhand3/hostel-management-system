04cb5509aa7032b11923d659b0c78bfe58cc45d6
rameshwarbhand3 <rameshwarbhand3@gmail.com>
2023-11-06 16:29:41 +0530

    User interface create and Integrate backend rest api

---
+++ Front-End/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <div>
+            <footer className='footer'>
+                <span>&copy; All Copyright is reserved by DEVELOPER</span>
+            </footer>            
+        </div>
+    )
+}
